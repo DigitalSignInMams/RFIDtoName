a91b6255847c99ffdcd941ba9e95deafe0a3d577
Charles Tang <ctang5@wpi.edu>
2022-11-07 23:35:44 -0500

    updated daily attendance thing to account for new names in student_info

--- updatestudents.py
+++ updatestudents.py
@@ -1,11 +1,10 @@
 import firebase_admin
 from firebase_admin import firestore
 from firebase_admin import credentials
+import json
 
-def initialize_student_info():
-    cred = credentials.Certificate('mass-academy-sign-in-system-60c92728ae76.json')
-    app = firebase_admin.initialize_app(cred)
-    db = firestore.client()
-
+def initialize_student_info(cred, app, db):
     doc = json.load(open('students.json', 'r'))
     db.collection('student_info').document('students').set(doc)
+    db.close()
+
